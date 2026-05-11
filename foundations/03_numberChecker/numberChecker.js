d39fce59a9ec176687f8c880733bcacaff957ed9
remen <remen@192.168.1.3>
2026-05-11 14:49:57 +0700

    exercices foundation 1-5

--- foundations/03_numberChecker/numberChecker.js
+++ foundations/03_numberChecker/numberChecker.js
@@ -1,9 +1,8 @@
 function numberChecker(number) {
-  if (number === 6) {
-    return true;
-  } else {
+    if (number >= 10) {
+        return true;
+    }
     return false;
-  }
 }
 
 // Do not edit below this line
