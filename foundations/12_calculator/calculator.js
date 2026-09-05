56615709b6bf2b7d1655b945fe529ca894a4418e
Feyr9z <muhammadariefkk@gmail.com>
2026-09-05 18:33:41 +0700

    calculator finishied

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,41 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  if (array.length === 0) {
+    return 0;
+  }
+
+  return array.reduce((sum, item) => sum + item, 0);
 };
 
-const multiply = function() {
+const multiply = function (array) {
+  if (array.length === 0) {
+    return 0;
+  }
 
+  return array.reduce((sum, item) => sum * item);
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
-};
+function factorial(n) {
+  // Reject negative numbers
+  if (n < 0) return "Undefined for negative numbers";
+
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +44,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
