4f8d72c5e6f517cdafd7eebc1353edba9e792c15
Feyr9z <muhammadariefkk@gmail.com>
2026-09-06 23:31:28 +0700

    fibonacci is finished

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (typeof n === "string" && !/^\d+$/.test(n)) {
+    return "OOPS";
+  }
 
+  n = Number(n);
+
+  if (n < 0) return "OOPS";
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  let firstTerm = 0;
+  let secondTerm = 1;
+
+  for (let i = 2; i <= n; i++) {
+    let nextTerm = firstTerm + secondTerm;
+
+    firstTerm = secondTerm;
+    secondTerm = nextTerm;
+  }
+
+  return secondTerm;
 };
 
 // Do not edit below this line
