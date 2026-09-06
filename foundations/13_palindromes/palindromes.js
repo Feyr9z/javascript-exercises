a7dade3b2443c2c819ca165b601e8103bf8d2320
Feyr9z <muhammadariefkk@gmail.com>
2026-09-06 14:58:50 +0700

    palindroms task is finished

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const filteredString = string
+    .toLowerCase()
+    .split("")
+    .filter((char) => {
+      return /[a-zA-Z0-9]/.test(char);
+    })
+    .join("");
 
+  const reverseString = filteredString.split("").reverse().join("");
+
+  return reverseString === filteredString;
 };
 
 // Do not edit below this line
