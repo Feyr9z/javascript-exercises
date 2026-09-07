7134cf9bbc6580aefba4b03c8ae2ce6024b93ce9
Feyr9z <muhammadariefkk@gmail.com>
2026-09-07 12:19:07 +0700

    findTheOldest is finished

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  return array.reduce((oldest, candidate) => {
+    const oldestAge =
+      oldest.yearOfDeath === undefined
+        ? new Date().getFullYear() - oldest.yearOfBirth
+        : oldest.yearOfDeath - oldest.yearOfBirth;
 
+    const candidateAge =
+      candidate.yearOfDeath === undefined
+        ? new Date().getFullYear() - candidate.yearOfBirth
+        : candidate.yearOfDeath - candidate.yearOfBirth;
+
+    if (candidateAge > oldestAge) oldest = candidate;
+
+    return oldest;
+  });
 };
 
 // Do not edit below this line
